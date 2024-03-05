ffda3740ccf07fe8877cffe856961986c835c85e
ibrada <radikibragimov@mail.ru>
2024-03-06 01:18:26 +0300

    Lesson31 fixed

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -2,7 +2,7 @@ package org.example.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import java.util.Objects;
 
